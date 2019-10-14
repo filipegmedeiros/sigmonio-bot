f175da3413157aa5360c49ccc8467530a938c74b
Filipe Medeiros <filipemedeiros@tuta.io>
2019-10-14 16:08:30 -0300

    Create an interface -> Repository LocalizationRepository
    and this allows to use all the methods of this class:
    https://docs.spring.io/spring-data/jpa/docs/current/api/org/springframework/data/jpa/repository/JpaRepository.html

---
+++ src/main/java/com/lp2/sigmonio/repository/LocalizationRepository.java
@@ -0,0 +1,11 @@
+package com.lp2.sigmonio.repository;
+
+
+import com.lp2.sigmonio.model.Localization;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LocalizationRepository extends JpaRepository<Localization, Long>{
+
+}
