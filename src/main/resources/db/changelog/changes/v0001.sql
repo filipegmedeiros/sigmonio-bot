5d3fba400a30042536380968e483ca32cda228c9
Filipe Medeiros <filipemedeiros@tuta.io>
2019-10-16 11:43:30 -0300

    add Item e Category to schema

--- src/main/resources/db/changelog/changes/v0001.sql
+++ src/main/resources/db/changelog/changes/v0001.sql
@@ -5,9 +5,17 @@ create table localization (
   primary key (id)
 );
 
+create table category (
+  id bigserial not null,
+  name varchar(50) not null,
+  description varchar(512) not null,
+  primary key (id)
+);
+
 create table item (
   id bigserial not null,
-  localization_id bigint not null references localization (id),
+  localization_id bigint references localization (id),
+  category_id bigint references category (id),
   name varchar(50) not null,
   description varchar(512) not null,
   primary key (id)
