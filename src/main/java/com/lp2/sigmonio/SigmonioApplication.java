e54ffa11c01e59e59f3c98eb2e75b6bd586c6dd5
Vinicius Santos <viniciussslima999@gmail.com>
2019-10-19 23:57:57 -0300

    Added initialization of telegram bot API

--- src/main/java/com/lp2/sigmonio/SigmonioApplication.java
+++ src/main/java/com/lp2/sigmonio/SigmonioApplication.java
@@ -1,8 +1,12 @@
 package com.lp2.sigmonio;
 
+import com.lp2.sigmonio.bot.SigmonioBot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -10,6 +14,14 @@ public class SigmonioApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SigmonioApplication.class, args);
-	}
 
+		ApiContextInitializer.init();
+
+		TelegramBotsApi api = new TelegramBotsApi();
+		try {
+			api.registerBot(new SigmonioBot());
+		} catch (TelegramApiRequestException e) {
+			e.printStackTrace();
+		}
+	}
 }
