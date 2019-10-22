3016525f98b8d925a656c63156eebc84c5242201
Filipe Medeiros <filipemedeiros@tuta.io>
2019-10-22 11:56:51 -0300

    Removing trash

--- src/main/java/com/lp2/sigmonio/SigmonioApplication.java
+++ src/main/java/com/lp2/sigmonio/SigmonioApplication.java
@@ -1,27 +1,26 @@
 package com.lp2.sigmonio;
 
-import com.lp2.sigmonio.bot.SigmonioBot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.telegram.telegrambots.ApiContextInitializer;
-import org.telegram.telegrambots.meta.TelegramBotsApi;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
+
+
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
 
 @SpringBootApplication
-@EnableJpaAuditing
+@EnableJpaRepositories
 public class SigmonioApplication {
+	@PostConstruct
+	void started(){
+		TimeZone.setDefault(TimeZone.getTimeZone("TimeZone"));
+	}
 
 	public static void main(String[] args) {
-		SpringApplication.run(SigmonioApplication.class, args);
 		ApiContextInitializer.init();
-
-	/*	ApiContextInitializer.init();
-
-		TelegramBotsApi api = new TelegramBotsApi();
-		try {
-			api.registerBot(new SigmonioBot());
-		} catch (TelegramApiRequestException e) {
-			e.printStackTrace();*/
-		}
+		SpringApplication.run(SigmonioApplication.class, args);
 	}
+
+}
